e47dfd819845b6d0d38c969616e0606127711bc2
Dina Muravska <dina.muravska.25@gmail.com>
2026-04-16 21:22:19 +0300

    fix styles

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,16 +5,16 @@ import AuthNavigation from "../AuthNavigation/AuthNavigation";
 export default function Header() {
   return (
     <header className={css.header}>
-      <Link href="/" aria-label="Home">
+      <Link href="/" aria-label="Home" className={css.headerLink}>
         NoteHub
       </Link>
-      <nav aria-label="Main Navigation" className={css.navigation}>
+      <nav aria-label="Main Navigation" className={css.nav}>
         <ul className={css.navigation}>
-          <li>
-            <Link href="/">Home</Link>
+          <li className={css.navigationItem}>
+            <Link href="/" className={css.navigationLink}>
+              Home
+            </Link>
           </li>
-        </ul>
-        <ul className={css.navigation}>
           <AuthNavigation />
         </ul>
       </nav>

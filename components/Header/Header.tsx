fba432e84d3a9055c818e39f549f465e46ee3b2f
Dina Muravska <dina.muravska.25@gmail.com>
2026-04-16 21:25:18 +0300

    return styles

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,16 +5,16 @@ import AuthNavigation from "../AuthNavigation/AuthNavigation";
 export default function Header() {
   return (
     <header className={css.header}>
-      <Link href="/" aria-label="Home" className={css.headerLink}>
+      <Link href="/" aria-label="Home">
         NoteHub
       </Link>
-      <nav aria-label="Main Navigation" className={css.nav}>
+      <nav aria-label="Main Navigation" className={css.navigation}>
         <ul className={css.navigation}>
-          <li className={css.navigationItem}>
-            <Link href="/" className={css.navigationLink}>
-              Home
-            </Link>
+          <li>
+            <Link href="/">Home</Link>
           </li>
+        </ul>
+        <ul className={css.navigation}>
           <AuthNavigation />
         </ul>
       </nav>
